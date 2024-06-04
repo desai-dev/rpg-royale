d53a0d5797aee54fddbcdd3067767c15665a181b
dev desai <dev_desai@outlook.com>
2024-06-03 22:37:24 -0400

    setup basic websocket server

---
+++ backend/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+// Upgrader to upgrade to websocket connection
+var upgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+}
+
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+				log.Println(err)
+				return
+		}
+
+		// print out that message for clarity
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+				log.Println(err)
+				return
+		}
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+    upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+    ws, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+    }
+
+    log.Println("Client Connected")
+    err = ws.WriteMessage(1, []byte("Hi Client!"))
+    if err != nil {
+			log.Println(err)
+    }
+
+    // Listen for messages
+    reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/ws", wsEndpoint)
+}
+
+func main() {
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
